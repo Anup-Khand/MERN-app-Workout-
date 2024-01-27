8b654a18a39bd72f735885ab0a9867cfdcc9da10
Anup-Khand <bk.basantkhand@gmail.com>
2024-01-27 12:32:14 +0545

    added jwt,hash in password and create auth context

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,47 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+// create json web token
+
+const createToken = (_id) => {
+    jwt.sign({_id},process.env.SECRET,{expiresIn:'3d'})
+}
 
 // login user
 
-const loginUser = async (req,res) => {
-    res.json({msg:'login user'})
+const loginUser = async (req, res) => {
+    
+    const { email, password } = req.body
+    
+      try {
+        const user = await User.login(email, password)
+
+        // get token
+        const token = createToken(user._id)
+
+        res.status(200).json({email,token})
+    } catch (error) {
+        res.status(400).json({msg:error.message})
+    }
+
 }
 
 //signup user
 
-const signupUser = async (req,res) => {
+const signupUser = async (req, res) => {
+    const { email, password } = req.body
+    
+    try {
+        const user = await User.signup(email, password)
+
+        // get token
+        const token = createToken(user._id)
+
+        res.status(200).json({email,token,user})
+    } catch (error) {
+        res.status(400).json({msg:error.message})
+    }
+
     res.json({msg:'login user'})
 }
 
